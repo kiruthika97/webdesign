38365e249484641e6aa601e0ec1db3567a500c53
kiruthika97 <65493911+kiruthika97@users.noreply.github.com>
2020-10-13 09:45:06 +0530

    php

---
+++ aboutme.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php
+function aboutme()
+{
+echo"I'M Fresher,i am completed my MCA in PET Engineering College in Valliyoor.I am completed my UG Degree Bsc(cs) in Govindammal Aditanar Womens College in Tiruchendur.My Basic skill Knowledge are Html,php,java script,c,c++,python basics.";
+}
+aboutme();
+?>
+</body>
+</html>
+
