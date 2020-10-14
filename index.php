03376dfebb49e1b71aee26ebec7dde12bd7a9031
kiruthika97 <65493911+kiruthika97@users.noreply.github.com>
2020-10-14 12:49:25 +0530

    php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<html style="background-color:#fff">
+<head>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta charset=UTF-8">
+<title>Website design</title>
+<link rel="stylesheet" href="style.css">
+<link rel="comment" href="aboutme.php">
+</head>
+<body>
+<?php
+<header>
+<a href="#" class="logo">Page</a>
+<div class="toggle"></div>
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="aboutme.php">About</a></li>
+</ul>
+</header>
+<selection class="banner">
+<div class="textBx">
+<br>
+<h2>Hello, I'm <br><space>KIRUTHIKA CHERMADURAI.</space></h2>
+<h3>I'm a Web Designer.</h3>
+<a href="#" class="btn" onclick="aboutme.php">About Me</a>
+</div>
+</selection>
+?>
+</body>
+</html>
