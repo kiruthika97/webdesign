b90c4b3c9a65ba815aa1d9d7d0a366797830a55f
kiruthika97 <65493911+kiruthika97@users.noreply.github.com>
2020-10-14 12:50:32 +0530

    changes aboutme

--- aboutme.php
+++ aboutme.php
@@ -1,12 +1,15 @@
 <!DOCTYPE HTML>
 <html>
+<head>
+<title>About</title>
+</head>
 <body>
 <?php
-function aboutme()
-{
-echo"I'M Fresher,i am completed my MCA in PET Engineering College in Valliyoor.I am completed my UG Degree Bsc(cs) in Govindammal Aditanar Womens College in Tiruchendur.My Basic skill Knowledge are Html,php,java script,c,c++,python basics.";
-}
-aboutme();
+<h1 style="text-align:center;">About Me</h1>
+<div style="margin-left:41%"><img src="images/flower.jpg" style="width:25%"></div>
+<p style="font-weight:bold;line-height:30px;">&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;I'M Fresher,i am completed my MCA in PET Engineering College in Valliyoor.I am completed my UG Degree Bsc(cs) in Govindammal Aditanar Womens College in Tiruchendur.My Basic skill Knowledge are Html,php,java script,c,c++,python basics...</p>
+<h1 style="text-align:center;color:blue;font-size:18px">Email-Id: kiruthikac97@gmail.com</h1>
+<h1 style="text-align:center;color:blue;font-size:18px">Mobile-Number: 7397641401</h1>
 ?>
 </body>
 </html>
